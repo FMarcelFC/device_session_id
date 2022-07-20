1ccfea6f1a96a131e0424aca549a83746be85336
Fortune Flores <l.flfc000720@itses.edu.mx>
2022-07-20 10:32:05 -0500

    Version 1.0.1 lista, correccion en documentacion

--- lib/device_session_id.dart
+++ lib/device_session_id.dart
@@ -5,15 +5,16 @@ import 'package:flutter/services.dart';
 
 class DeviceSessionId {
   @visibleForTesting
-  static const methodChannel = MethodChannel('device_session_id');
+  static const methodChannel = MethodChannel(
+      'device_session_id'); // METHOD FOR CALLING THE NATIVE CODE LIBRARIES
 
   static Future<String?> getDeviceID(
       String MERCHANT_ID, String API_KEY, bool productionMode) async {
     final deviceID = await methodChannel.invokeMethod<String>('getDeviceID', {
-      'MERCHANT_ID': MERCHANT_ID,
-      'API_KEY': API_KEY,
-      'productionMode': productionMode
+      'MERCHANT_ID': MERCHANT_ID, // MERCHANT ID GIVEN BY OPENPAY
+      'API_KEY': API_KEY, // API KEY GIVEN BY OPENPAY
+      'productionMode': productionMode // PRODUCTION MODE OR DEBUG MODE
     });
-    return deviceID;
+    return deviceID; // RETURNS DEVICE SESSION ID AFTER RUNNING THROUGH OPENPAY LIBRARIES
   }
 }
